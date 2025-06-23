fcffe5c6059cac376c53ef4f6a5d7d30a37f249f
Usama Ahmed Siddiquie <220usamaahmed@gmail.com>
2025-06-23 13:03:58 +0200

    Optimize arrays

--- dataset.py
+++ dataset.py
@@ -34,35 +34,50 @@ class MujocoDataset:
         self._random = random
         self._window_size = window_size
 
-        self._obs = []
-        self._acts = []
-
         self._load_dataset()
 
     def __len__(self):
-        return len(self._obs)
+        return self._obs.shape[0]
 
     def __getitem__(self, idx):
         return (
-            self._obs[idx],
-            self._acts[idx],
+            self._obs[idx, :, :],
+            self._acts[idx, :, :],
         )
 
     def _load_dataset(self):
         print(f"Loading dataset from {self._save_loc}")
-        if (self._save_loc / "obs.pkl").exists() and (
-            self._save_loc / "acts.pkl"
-        ).exists():
-            print("Found pickled data files, loading...")
-            with open(self._save_loc / "obs.pkl", "rb") as f:
-                self._obs = pickle.load(f)
-            with open(self._save_loc / "acts.pkl", "rb") as f:
-                self._acts = pickle.load(f)
+        obs_path = self._save_loc / "obs.npy"
+        acts_path = self._save_loc / "acts.npy"
+        if obs_path.exists() and acts_path.exists():
+            print("Found npy data files, loading...")
+            self._obs = np.load(obs_path)
+            self._acts = np.load(acts_path)
         else:
-            print("Pickled data files not found, building dataset...")
+            print("Npy data files not found, building dataset...")
             self._build_dataset()
 
+    def _initialize_arrays(self):
+        if self._random:
+            env = gym.make(self._env_name, terminate_when_unhealthy=False)
+            obs_len = env.observation_space.shape[0]
+            acts_len = env.action_space.shape[0]
+        else:
+            dataset = minari.load_dataset(
+                f"mujoco/{self._env_name}/{self._tag}", download=True
+            )
+            episode = next(dataset.iterate_episodes())
+            obs_len = len(episode.observations[0])
+            acts_len = len(episode.actions[0])
+
+        self._max_trajs = 2000
+        self._obs = np.zeros((self._max_trajs, self._window_size, obs_len))
+        self._acts = np.zeros((self._max_trajs, self._window_size, acts_len))
+        self._traj_added = 0
+
     def _build_dataset(self):
+        self._initialize_arrays()
+
         print(f"Building dataset for mujoco/{self._env_name}/{self._tag}")
         if self._random:
             self._build_random_dataset()
@@ -74,14 +89,15 @@ class MujocoDataset:
             f"mujoco/{self._env_name}/{self._tag}", download=True
         )
 
-        total_timesteps = 1e5
+        total_timesteps = 1e4
         current_timestep = 0
 
         for episode in dataset.iterate_episodes():
             current_timestep += len(episode.observations)
             
             print("Adding data from episode...")
-            self._add_from_episode(episode)
+            if self._add_from_episode(episode):
+                break
 
             if current_timestep > total_timesteps:
                 break
@@ -92,27 +108,22 @@ class MujocoDataset:
     def _build_random_dataset(self):
         print(f"Building random dataset for mujoco/{self._env_name}/{self._tag}")
         env = gym.make(self._env_name, terminate_when_unhealthy=False)
-        total_steps = 1e5
-        current_step = 0
         while True:
-            if current_step > total_steps:
-                print("stopping at ", current_step)
-                break
             obs = env.reset()[0]
             done = False
             observations = []
             actions = []
             while not done:
-                current_step += 1
                 action = env.action_space.sample()
                 observations.append(obs)
                 actions.append(action)
                 obs, reward, terminated, truncated, info = env.step(action)
-                done = terminated or truncated or current_step > total_steps
+                done = terminated or truncated
             episode = Buffer()
             episode.observations = observations
             episode.actions = actions
-            self._add_from_episode(episode)
+            if self._add_from_episode(episode):
+                break
         env.close()
         self._save_dataset()
 
@@ -137,18 +148,24 @@ class MujocoDataset:
             obs_list = list(obs_de)
             acts_list = list(acts_de)
 
-            self._obs.append(np.array(obs_list))
-            self._acts.append(np.array(acts_list))
-        print(f"Added {len(self._obs)} obs, {len(self._acts)} acts.")
+            self._obs[self._traj_added, :, :] = np.array(obs_list)
+            self._acts[self._traj_added, :, :] = np.array(acts_list)
+            self._traj_added += 1
+            if self._traj_added >= self._max_trajs:
+                return True
+
+        print(f"Added {self._obs.shape[0]} trajectories")
 
     def _save_dataset(self):
+        if self._traj_added < self._max_trajs:
+            self._obs = self._obs[:self._traj_added, :, :]
+            self._acts = self._acts[:self._traj_added, :, :]
+
         print(f"Saving dataset to {self._save_loc}")
         makedirs(self._save_loc, exist_ok=True)
 
-        with open(self._save_loc / "obs.pkl", "wb") as f:
-            pickle.dump(self._obs, f)
-        with open(self._save_loc / "acts.pkl", "wb") as f:
-            pickle.dump(self._acts, f)
+        np.save(self._save_loc / "obs.npy", self._obs)
+        np.save(self._save_loc / "acts.npy", self._acts)
         print("Dataset saved and config.yaml updated.")
 
 
@@ -164,3 +181,5 @@ if __name__ == "__main__":
     print(act.shape, act.dtype)
 
     print(obs[:25])
+
+    print(d._obs.shape, d._acts.shape)
